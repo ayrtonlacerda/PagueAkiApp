49fe441fe7990f79da1a3f1dcf7d051a6c8087b8
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2020-04-28 21:31:00 -0300

    fix icon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
+
+MaterialIcons.loadFont();
 
 function App() {
   return (
